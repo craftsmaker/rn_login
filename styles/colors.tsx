f3e7779cf0b08de553afa577c3746b2abbcef9a6
Victor Guedes <victorguedes68@hotmail.com>
2020-06-17 16:14:43 -0300

    Login improvements

---
+++ styles/colors.tsx
@@ -0,0 +1,9 @@
+export default {
+    facebookBackgroundColor: "#D7D2F9",
+    facebookColor: "#3A559f",
+    googleBackgroundColor: "#FCD2CD",
+    googleColor: "#DD4B39",
+    linkColor: "#5F4AE7",
+    bottomGradientColor: "#6A49E4",
+    topGradientColor: "#7C47DE",
+}
